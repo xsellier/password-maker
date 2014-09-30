eb21786bd49daaf52a70501dd8f9d36c82d15e28
xsellier <xsellier@can2go.com>
2014-09-30 14:59:43 -0400

    Final release
    - generate a beautiful password
    - don't overwrite generated letter (except lowercases)
    - add unit test

--- README.md
+++ README.md
@@ -2,3 +2,41 @@ password-maker
 ==============
 
 Generate a password randomly (optimized).
+
+## Usage
+password-maker can be used like described below:
+
+``` js
+  var passwordMaker = require("password0maker");
+
+  var options = {
+    uppercase: false,
+    symbols  : false,
+    numbers  : true
+  };
+
+  var password = passwordMaker.generatePassword(options, 32);
+```
+
+## Installation
+
+### Installing password-maker
+```
+  npm install password-maker --save
+```
+
+## Run Tests
+Tests are written with mocha/chai.
+
+``` bash
+  $ npm test
+```
+
+## Changelog
+0.1.1
+
+- Final release
+
+0.1.0
+
+- First release, everything was hard-coded
--- test/password-maker-test.js
+++ test/password-maker-test.js
@@ -4,8 +4,71 @@ var passwordMaker = require("../lib/index")
   , expect        = chai.expect;
 
 describe("The password-maker", function() {
-  it("should generate a password", function(done) {
-    expect(passwordMaker.generatePassword()).to.exist;
+  it("should generate a password of length 50", function(done) {
+    var result = passwordMaker.generatePassword({}, 50);
+    expect(result.length).to.equal(50);
+    done();
+  });
+
+  it("should generate a password with only lower cases", function(done) {
+    var options = {
+      uppercase: false,
+      symbols  : false,
+      numbers  : false
+    };
+
+    var result = passwordMaker.generatePassword(options);
+    expect(result).to.match(/^[a-z]+$/);
+    done();
+  });
+
+  it("should generate a password with lower and upper cases", function(done) {
+    var options = {
+      uppercase: true,
+      symbols  : false,
+      numbers  : false
+    };
+
+    var result = passwordMaker.generatePassword(options);
+    expect(result).to.match(/[A-Z]/);
+    done();
+  });
+
+  it("should generate a password with lower cases and symbols", function(done) {
+    var options = {
+      uppercase: false,
+      symbols  : true,
+      numbers  : false
+    };
+
+    var result = passwordMaker.generatePassword(options);
+    expect(result).to.match(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/);
+    done();
+  });
+
+  it("should generate a password with lower cases and numbers", function(done) {
+    var options = {
+      uppercase: false,
+      symbols  : false,
+      numbers  : true
+    };
+
+    var result = passwordMaker.generatePassword(options);
+    expect(result).to.match(/[0-9]/);
+    done();
+  });
+
+  it("should generate a password with lower, upper cases, symbols and numbers", function(done) {
+    var options = {
+      uppercase: true,
+      symbols  : true,
+      numbers  : true
+    };
+
+    var result = passwordMaker.generatePassword(options);
+    expect(result).to.match(/[A-Z]/);
+    expect(result).to.match(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/);
+    expect(result).to.match(/[0-9]/);
     done();
   });
 });
